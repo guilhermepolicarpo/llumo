73e691e1c9ec4ca9cfff3a34c2ff3cd9d1e36177
GuilhermePolicarpo <guilhermepolicarpo@outlook.com>
2026-09-05 18:58:49 -0300

    feat: apply bold font to sidebar brand and clean up app logo component

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,7 +1,7 @@
 @props([
     'sidebar' => false,
 ])
-{{-- fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" --}}
+
 @if($sidebar)
     <flux:sidebar.brand :name="config('app.name', 'Laravel')" {{ $attributes }}>
         <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
