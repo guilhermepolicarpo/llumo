c4c9f4765076fe5d997c8a297ea1eb3be6150503
GuilhermePolicarpo <guilhermepolicarpo@outlook.com>
2026-09-07 14:29:53 -0300

    feat: add Blaze optimization in AppServiceProvider and create .gitignore for Blaze traces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Livewire\Blaze\Blaze;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        Blaze::optimize()->in(resource_path('views/components'));
     }
 
     /**
